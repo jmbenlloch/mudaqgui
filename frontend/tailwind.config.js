ab1e3f7329bc027ff04f55692bf931c3595b0974
jmbenlloch <info@jmbenlloch.net>
2023-09-11 10:26:20 +0200

    Add tailwind and daisyUI

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: ["light"],
+  },
+  plugins: [
+	  require("daisyui"),
+  ],
+}
+
