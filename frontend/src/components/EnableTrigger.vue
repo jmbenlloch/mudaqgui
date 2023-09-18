b4f6a28d1dec559ef66d2a0f065f5ba813955003
jmbenlloch <info@jmbenlloch.net>
2023-09-18 13:48:05 +0200

    Enable or32 UI

--- frontend/src/components/EnableTrigger.vue
+++ frontend/src/components/EnableTrigger.vue
@@ -12,7 +12,7 @@ const { slowControl } = storeToRefs(store)
 watch(triggerEnable, (value) => {
   console.log(value)
   const array: Array<number> = Array(nChannels.value).fill(0)
-  for (let ch of triggerEnable.value){
+  for (let ch of triggerEnable.value) {
     console.log(ch)
     array[ch] = 1
   }
@@ -29,10 +29,17 @@ watch(triggerEnable, (value) => {
     <div class="border border-primary w-fit grid grid-cols-4 gap-2">
       <div class="form-control" v-for="n in nChannels">
         <label class="label cursor-pointer">
-          <input v-model="triggerEnable" type="checkbox" class="checkbox" :value="n-1"/>
-          <span class="label-text">Ch. {{ n }}</span> 
+          <input v-model="triggerEnable" type="checkbox" class="checkbox" :value="n - 1" />
+          <span class="label-text">Ch. {{ n }}</span>
         </label>
       </div>
+    </div>
+
+    <div class="form-control">
+      <label class="label cursor-pointer">
+        <input v-model="slowControl.enable_or32" type="checkbox" class="checkbox" :true-value="1" :false-value="0" />
+        <span class="label-text text-lg">Trigger OR 32</span>
+      </label>
     </div>
   </div>
 </template>
