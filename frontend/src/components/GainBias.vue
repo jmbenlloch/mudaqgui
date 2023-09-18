29330e84173012bddee23e21ebc00607e7963c3e
jmbenlloch <info@jmbenlloch.net>
2023-09-18 13:37:49 +0200

    Save bias to pinia

--- frontend/src/components/GainBias.vue
+++ frontend/src/components/GainBias.vue
@@ -1,21 +1,20 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue'
+import { ref } from 'vue'
 import NumericInput from './NumericInput.vue';
 import { useConfigStore } from '../stores/configuration'
 import { storeToRefs } from 'pinia'
-import { update } from 'lodash';
 
 const nChannels = ref(32)
-const value = ref(0)
 
 const store = useConfigStore()
 const { slowControl } = storeToRefs(store)
 
-function updateValue(value: Event, index: number){
-  console.log("update")
-  console.log(value)
-  console.log(index)
+function updateGain(value: number, index: number) {
   slowControl.value.channel_preamp_HG[index] = value
+}
+
+function updateBias(value: number, index: number) {
+  slowControl.value.input_dac[index] = value
 }
 </script>
 
@@ -25,12 +24,13 @@ function updateValue(value: Event, index: number){
     <div class="border border-primary w-fit grid grid-cols-4 gap-2">
       <div class="form-control" v-for="(n, index) in nChannels">
         <label class="label cursor-pointer">
-          <span class="label-text">Ch. {{ n }}</span> 
-          <NumericInput :value="value" @update-value="updateValue($event, index)" class="mx-1" :min="0" :max="63"/>
-          <!-- <NumericInput class="mx-1" :min="0" :max="254"/> -->
+          <span class="label-text">Ch. {{ n }}</span>
+          <NumericInput :value="slowControl.channel_preamp_HG[index]" @update-value="updateGain($event, index)"
+            class="mx-1" :min="0" :max="63" />
+          <NumericInput :value="slowControl.input_dac[index]" @update-value="updateBias($event, index)" class="mx-1"
+            :min="0" :max="254" />
         </label>
       </div>
     </div>
   </div>
-  {{  slowControl.channel_preamp_HG }}
 </template>
