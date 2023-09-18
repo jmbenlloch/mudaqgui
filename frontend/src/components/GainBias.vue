38133023c05a83f2b75fdf5b0d3ba49854926d23
jmbenlloch <info@jmbenlloch.net>
2023-09-18 13:33:32 +0200

    Save gain to pinia state

--- frontend/src/components/GainBias.vue
+++ frontend/src/components/GainBias.vue
@@ -1,23 +1,36 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import NumericInput from './NumericInput.vue';
+import { useConfigStore } from '../stores/configuration'
+import { storeToRefs } from 'pinia'
+import { update } from 'lodash';
 
-const probeRegister = ref([])
 const nChannels = ref(32)
+const value = ref(0)
 
+const store = useConfigStore()
+const { slowControl } = storeToRefs(store)
+
+function updateValue(value: Event, index: number){
+  console.log("update")
+  console.log(value)
+  console.log(index)
+  slowControl.value.channel_preamp_HG[index] = value
+}
 </script>
 
 <template>
   <div>
     <h2 class="font-bold text-xl">HG preamp gain/bias</h2>
     <div class="border border-primary w-fit grid grid-cols-4 gap-2">
-      <div class="form-control" v-for="n in nChannels">
+      <div class="form-control" v-for="(n, index) in nChannels">
         <label class="label cursor-pointer">
           <span class="label-text">Ch. {{ n }}</span> 
-          <NumericInput class="mx-1" :min="0" :max="63"/>
-          <NumericInput class="mx-1" :min="0" :max="254"/>
+          <NumericInput :value="value" @update-value="updateValue($event, index)" class="mx-1" :min="0" :max="63"/>
+          <!-- <NumericInput class="mx-1" :min="0" :max="254"/> -->
         </label>
       </div>
     </div>
   </div>
+  {{  slowControl.channel_preamp_HG }}
 </template>
--- frontend/src/components/NumericInput.vue
+++ frontend/src/components/NumericInput.vue
@@ -1,25 +1,42 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
+import { debounce } from 'lodash';
 
+
+const emit = defineEmits(['updateValue'])
 const props = defineProps({
+  value: { type: Number, required: true },
   min: { type: Number, required: true },
   max: { type: Number, required: true },
 })
 
-const value = ref(0)
+const value = ref(props.value)
 
-function increment(){
-  if (value.value < props.max){
+function increment() {
+  if (value.value < props.max) {
     value.value++
   }
 }
 
-function decrement(){
-  if (value.value > props.min){
+function decrement() {
+  if (value.value > props.min) {
     value.value--
   }
 }
 
+watch(value, (newValue) => {
+  if (value.value > props.max) {
+    value.value = props.max
+  }
+
+  if (value.value < props.min) {
+    value.value = props.min
+  }
+
+  debounce(() => {
+    emit("updateValue", newValue)
+  }, 100)()
+})
 </script>
 
 <template>
@@ -29,12 +46,13 @@ function decrement(){
         class=" bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-l cursor-pointer outline-none">
         <span class="m-auto text-2xl font-thin">−</span>
       </button>
-      <input type="number" min="0" max="64"
+      <input type="number" :min="props.min" :max="props.max"
         class="outline-none focus:outline-none text-center w-full bg-gray-300 hover:text-black focus:text-black  md:text-basecursor-default flex items-center text-gray-700"
         name="custom-input-number" v-model="value" />
       <button @click="increment"
         class="bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-r cursor-pointer">
         <span class="m-auto text-2xl font-thin">+</span>
       </button>
+    </div>
   </div>
-</div></template>
+</template>
