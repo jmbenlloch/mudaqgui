5b5dd8aa4dbafa50547414598512410755ddc1c5
jmbenlloch <info@jmbenlloch.net>
2023-09-19 20:00:12 +0200

    First plot using plotly

--- frontend/src/components/Events.vue
+++ frontend/src/components/Events.vue
@@ -1,31 +1,24 @@
 <script setup lang="ts">
 import { onMounted, ref, watch } from 'vue'
+// TODO: solve issue with plotly.js-dist types. Currently:
+// mv node_modules/@types/plotly.js node_modules/@types/plotly.js-dist
+import Plotly from 'plotly.js-dist'
 import { EventsOn } from '../../wailsjs/runtime/runtime'
-import * as d3 from "d3";
 
-const width = ref(640)
-const height = ref(400)
-const marginTop = ref(20)
-const marginRight = ref(20)
-const marginBottom = ref(20)
-const marginLeft = ref(2)
-
-const data = ref([1, 2, 3, 4, 5, 6, 7])
-
-const x = d3.scaleLinear([0, data.value.length - 1], [marginLeft.value, width.value - marginRight.value]);
-const y = d3.scaleLinear(d3.extent(data.value), [height.value - marginBottom.value, marginTop.value]);
-const line = d3.line((d, i) => x(i), y);
-
-const yAxis = d3.scaleLinear()
-  .domain([0, 100])
-  .range([height.value - marginBottom.value, marginTop.value]);
-
-
-
+const test = ref<HTMLElement>()
 onMounted(() => {
   EventsOn("events", (data) => {
     console.log("events", data)
   })
+
+  Plotly.newPlot(test.value as HTMLElement, [{
+    x: [1, 2, 3, 4, 5],
+    y: [1, 2, 4, 8, 16]
+  }], {
+    margin: { t: 0 }
+  });
+
+
 })
 
 </script>
@@ -33,12 +26,6 @@ onMounted(() => {
 <template>
   <div>
     <h1>Test</h1>
-    <svg :width="width" :height="height">
-      <path fill="none" stroke="currentColor" stroke-width="1.5" :d="line(data)" />
-      <g fill="white" stroke="currentColor" stroke-width="1.5">
-        {data.map((d, i) => (
-        <circle key={i} cx={x(i)} cy={y(d)} r="2.5" />))}
-      </g>
-    </svg>
+    <div ref="test" style="width:600px;height:250px;"></div>
   </div>
 </template>
