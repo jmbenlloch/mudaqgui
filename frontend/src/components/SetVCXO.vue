f38d598cb31209101094ba1a425907e234e3c0ed
jmbenlloch <info@jmbenlloch.net>
2023-09-19 11:24:22 +0200

    Add VCXO UI

---
+++ frontend/src/components/SetVCXO.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+import NumericInput from './NumericInput.vue';
+
+const vcxo = ref(0)
+
+function updateVXCO(value: number){
+  console.log("Do something VCXO")
+}
+</script>
+
+<template>
+  <div>
+    <h2 class="font-bold text-xl">DAC threshold</h2>
+    <div class="border border-primary w-fit px-3">
+      <div class="form-control max-w-xs">
+        <label class="label">
+          <span class="label-text">DAC Threshold</span>
+        </label>
+        <NumericInput :value="vcxo" @update-value="updateVXCO" class="mx-1"
+          :min="0" :max="1024" />
+      </div>
+    </div>
+  </div>
+</template>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -5,6 +5,7 @@ import ProbeRegister from '../components/ProbeRegister.vue'
 import GainBias from '../components/GainBias.vue';
 import MenuBar from '../components/MenuBar.vue';
 import SetDAC from '@/components/SetDAC.vue';
+import SetVCXO from '@/components/SetDAC.vue';
 </script>
 
 <template>
@@ -21,6 +22,7 @@ import SetDAC from '@/components/SetDAC.vue';
         <div class="flex flex-col gap-3">
           <ProbeRegister />
           <SetDAC />
+          <SetVCXO />
         </div>
           <GainBias />
       </div>
